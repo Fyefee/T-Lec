5138371c6ec2e8c2925d54bd32efbc8012b84bca
Felzj <jeffychawin@gmail.com>
2021-10-10 22:41:20 +0700

    Edit border spinning

--- frontend/src/screens/Login.js
+++ frontend/src/screens/Login.js
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react'
-import { StyleSheet, Text, View, Dimensions, Easing, Image, Animated,  PixelRatio, Platform } from 'react-native'
+import { StyleSheet, Text, View, Dimensions, Easing, Image, Animated, PixelRatio, Platform } from 'react-native'
 import * as Font from 'expo-font';
 
 let customFonts = {
-    'Kanit': require('../assets/fonts/Kanit-Medium.ttf'),
-  };
+    'Kanit': require('../assets/fonts/Kanit-Light.ttf'),
+};
 
 const {
     width: SCREEN_WIDTH,
@@ -14,7 +14,7 @@ const {
 const scale = SCREEN_WIDTH / 320;
 
 const normalize = (size) => {
-    const newSize = size * scale 
+    const newSize = size * scale
     if (Platform.OS === 'ios') {
         return Math.round(PixelRatio.roundToNearestPixel(newSize))
     } else {
@@ -26,45 +26,57 @@ export default function App() {
     Font.loadAsync(customFonts);
     const spinValue = new Animated.Value(0);
     const spinValue2 = new Animated.Value(0);
+    const spinValue3 = new Animated.Value(0);
     const star1MoveValue = new Animated.Value(0);
 
     useEffect(() => {
         Animated.loop(
-        Animated.parallel([
-            Animated.timing(
-                spinValue,
-                {
-                    toValue: 1,
-                    duration: 3000,
-                    easing: Easing.linear,
-                    useNativeDriver: true,
-                    isInteraction: false,
-                    iterations: -1
-                }
-            ),
-            Animated.timing(
-                spinValue2,
-                {
-                    toValue: 1,
-                    duration: 2500,
-                    easing: Easing.elastic(),
-                    useNativeDriver: true,
-                    isInteraction: false,
-                    iterations: -1
-                }
-            ),
-            Animated.timing(
-                star1MoveValue,
-                {
-                    toValue: 1,
-                    duration: 2000,
-                    easing: Easing.linear,
-                    useNativeDriver: true,
-                    isInteraction: false,
-                    iterations: -1
-                }
-            )])
-    ).start();
+            Animated.parallel([
+                Animated.timing(
+                    spinValue,
+                    {
+                        toValue: 1,
+                        duration: 15000,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                        isInteraction: false,
+                        iterations: -1
+                    }
+                ),
+                Animated.timing(
+                    spinValue2,
+                    {
+                        toValue: 1,
+                        duration: 2500,
+                        easing: Easing.elastic(),
+                        useNativeDriver: true,
+                        isInteraction: false,
+                        iterations: -1
+                    }
+                ),
+                Animated.timing(
+                    spinValue3,
+                    {
+                        toValue: 1,
+                        duration: 35000,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                        isInteraction: false,
+                        iterations: -1
+                    }
+                ),
+                Animated.timing(
+                    star1MoveValue,
+                    {
+                        toValue: 1,
+                        duration: 2000,
+                        easing: Easing.linear,
+                        useNativeDriver: true,
+                        isInteraction: false,
+                        iterations: -1
+                    }
+                )])
+        ).start();
     }, [spinValue, spinValue2, star1MoveValue])
 
     const spin = spinValue.interpolate({
@@ -73,6 +85,11 @@ export default function App() {
     })
 
     const spin2 = spinValue2.interpolate({
+        inputRange: [0, 1],
+        outputRange: ['0deg', '-360deg']
+    })
+
+    const spin3 = spinValue3.interpolate({
         inputRange: [0, 1],
         outputRange: ['0deg', '360deg']
     })
@@ -88,13 +105,17 @@ export default function App() {
 
     return (
         <View style={styles.container}>
-            {/* <Animated.View style={[styles.circleBorder2, { transform: [{ rotate: spin2 }] }]} /> */}
+            {/* <Animated.View style={[styles.circleBorder, { transform: [{ rotate: spin }] }]} />
+            <Animated.View style={[styles.circleBorder2, { transform: [{ rotate: spin2 }] }]} /> */}
+            <Animated.Image style={[styles.circleBorder, { transform: [{ rotate: spin }] }]} source={require("../assets/Login_pic_frame/frame1.png")}></Animated.Image>
+            <Animated.Image style={[styles.circleBorder2, { transform: [{ rotate: spin2 }] }]} source={require("../assets/Login_pic_frame/frame2.png")}></Animated.Image>
+            <Animated.Image style={[styles.circleBorder3, { transform: [{ rotate: spin3 }] }]} source={require("../assets/Login_pic_frame/frame3.png")}></Animated.Image>
             <View style={styles.logoContainer}>
                 <Image style={styles.logo} source={require("../assets/logo/logo_color_dino.png")}></Image>
                 <Text style={styles.loginText}>Login</Text>
             </View>
             <Animated.Image style={[styles.star_decration1, { transform: [{ translateY: move_star1 }] }]} source={require("../assets/decoration/star2.png")}></Animated.Image>
-            <Animated.Image style={[styles.star_decration1, { transform: [{ translateY: move_star1 }] }]} source={require("../assets/decoration/star.png")}></Animated.Image>
+            <Animated.Image style={[styles.star_decration2, { transform: [{ translateY: move_star1 }] }]} source={require("../assets/decoration/star.png")}></Animated.Image>
         </View>
     );
 
@@ -107,22 +128,29 @@ const styles = StyleSheet.create({
         backgroundColor: '#FFB085',
         alignItems: 'center',
         padding: 20,
-        justifyContent: 'center'
+        justifyContent: 'center',
+        overflow: "hidden"
     },
     logoContainer: {
         borderRadius: Math.round(Dimensions.get('window').width + Dimensions.get('window').height) / 2,
-        width: Dimensions.get('window').width * 0.7,
-        height: Dimensions.get('window').width * 0.7,
+        // width: Dimensions.get('window').width * 0.7,
+        // height: Dimensions.get('window').width * 0.7,
+        width: Dimensions.get('window').width * 0.5,
+        height: Dimensions.get('window').width * 0.5,
         backgroundColor: '#FEF1E6',
         justifyContent: 'center',
         alignItems: 'center',
     },
     logo: {
-        width: (Dimensions.get('window').width * 0.5),
-        height: Dimensions.get('window').width * 0.5,
+        // width: (Dimensions.get('window').width * 0.5),
+        // height: Dimensions.get('window').width * 0.5,
+        width: (Dimensions.get('window').width * 0.35),
+        height: Dimensions.get('window').width * 0.35,
         position: "absolute",
-        top: (Dimensions.get('window').width * 0.04),
-        left: Dimensions.get('window').width * 0.125,
+        // top: (Dimensions.get('window').width * 0.04),
+        // left: Dimensions.get('window').width * 0.125,
+        top: (Dimensions.get('window').width * 0.025),
+        left: Dimensions.get('window').width * 0.1,
     },
     loginText: {
         position: "relative",
@@ -132,11 +160,11 @@ const styles = StyleSheet.create({
         fontWeight: "bold"
     },
     star_decration1: {
-        width: (Dimensions.get('window').height * 0.16),
-        height: Dimensions.get('window').height * 0.16,
+        width: (Dimensions.get('window').height * 0.135),
+        height: Dimensions.get('window').height * 0.135,
         position: "absolute",
-        top: (Dimensions.get('window').height * 0.1),
-        left: Dimensions.get('window').width * 0.05,
+        top: (Dimensions.get('window').height * 0.125),
+        left: Dimensions.get('window').width * 0.7,
     },
     star_decration2: {
         width: (Dimensions.get('window').height * 0.16),
@@ -144,5 +172,23 @@ const styles = StyleSheet.create({
         position: "absolute",
         top: (Dimensions.get('window').height * 0.1),
         left: Dimensions.get('window').width * 0.05,
+    },
+    circleBorder:{
+        width: (Dimensions.get('window').width * 0.575),
+        height: Dimensions.get('window').width * 0.575,
+        position: "absolute",
+        alignSelf: 'center',
+    },
+    circleBorder2:{
+        width: (Dimensions.get('window').width * 0.665),
+        height: Dimensions.get('window').width * 0.665,
+        position: "absolute",
+        alignSelf: 'center',
+    },
+    circleBorder3:{
+        width: (Dimensions.get('window').width * 0.685),
+        height: Dimensions.get('window').width * 0.685,
+        position: "absolute",
+        alignSelf: 'center',
     }
 });
